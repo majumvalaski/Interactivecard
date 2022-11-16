994025fa7f3077acfd17c7c1441f960a9f95a132
Maria Julia Valaski <maju.m.valaski@gmail.com>
2022-11-16 21:00:14 -0200

    JavaScript functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
+document.querySelector('.card-number').oninput = () =>{
+    document.querySelector('card-number-box').innerText = document.querySelector('.card-number').value;
+}
 
- 
+document.querySelector('.name').oninput = () =>{
+    document.querySelector('card-holder-name').innerText = document.querySelector('.name').value;
+}
+
+document.querySelector('.month-input').oninput = () =>{
+    document.querySelector('exp-month').innerText = document.querySelector('.month-input').value;
+}
+
+document.querySelector('.year-input').oninput = () =>{
+    document.querySelector('exp-year').innerText = document.querySelector('.year-input').value;
+}
