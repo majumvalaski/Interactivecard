789e28860a0164991df6851956e4972346b113a0
Maria Julia Valaski <maju.m.valaski@gmail.com>
2022-11-23 21:58:25 -0200

    whatever

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    /*modal.classList.toggle("hide");
+    fade.classList.toggle("hide"); */
+    /*jeito mais compacto de escrever linha 27 e 28*/
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
+}
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
+
 document.querySelector('.card-number').oninput = () =>{
     document.querySelector('card-number-box').innerText = document.querySelector('.card-number').value;
 }
@@ -18,18 +34,3 @@ document.querySelector('.code').oninput = () =>{
     document.querySelector('cvc').innerText = document.querySelector('.code').value;
 }
 
-const openModalButton = document.querySelector("#open-modal");
-const closeModalButton = document.querySelector("#close-modal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
-
-const toggleModal = () => {
-    /*modal.classList.toggle("hide");
-    fade.classList.toggle("hide"); */
-    /*jeito mais compacto de escrever linha 27 e 28*/
-    [modal, fade].forEach((el) => el.classList.toggle("hide"));
-}
-
-[openModalButton, closeModalButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());
-});
