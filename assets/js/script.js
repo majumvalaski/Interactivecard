7003982166e23314375e24a84d08d846caa8482e
Maria Julia Valaski <maju.m.valaski@gmail.com>
2022-11-23 21:08:50 -0200

    Deleting border of footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,14 @@ const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");
 const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    /*modal.classList.toggle("hide");
+    fade.classList.toggle("hide"); */
+    /*jeito mais compacto de escrever linha 27 e 28*/
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
+}
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
