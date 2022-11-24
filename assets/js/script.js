9714a2795d35fa5fba80c3bec1be625a775f18c3
Maria Julia Valaski <maju.m.valaski@gmail.com>
2022-11-24 21:09:14 -0200

    details on css

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,23 +14,24 @@ const toggleModal = () => {
     el.addEventListener("click", () => toggleModal());
 });
 
-document.querySelector('.card-number').oninput = () =>{
-    document.querySelector('card-number-box').innerText = document.querySelector('.card-number').value;
+
+document.querySelector('.inputTemplateNumber').oninput = () =>{
+    document.querySelector('.cardNumberFront').innerText = document.querySelector('.inputTemplateNumber').value;
 }
 
-document.querySelector('.name').oninput = () =>{
-    document.querySelector('card-holder-name').innerText = document.querySelector('.name').value;
+document.querySelector('.inputTemplateName').oninput = () =>{
+    document.querySelector('.cardholderNameFront').innerText = document.querySelector('.inputTemplateName').value;
 }
 
-document.querySelector('.month-input').oninput = () =>{
+/* document.querySelector('.month-input').oninput = () =>{
     document.querySelector('exp-month').innerText = document.querySelector('.month-input').value;
 }
 
 document.querySelector('.year-input').oninput = () =>{
     document.querySelector('exp-year').innerText = document.querySelector('.year-input').value;
-}
+} */
 
 document.querySelector('.code').oninput = () =>{
-    document.querySelector('cvc').innerText = document.querySelector('.code').value;
+    document.querySelector('.cvc').innerText = document.querySelector('.code').value;
 }
 
